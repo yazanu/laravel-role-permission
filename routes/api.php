4df5812e6a3c0914eb40be2c7e142f6444aa37eb
yuzana <34826444+yazanu@users.noreply.github.com>
2024-06-30 21:51:41 +0630

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -23,4 +23,7 @@ use App\Http\Controllers\Api\AuthController;
 Route::post('register', [AuthController::class, 'register']);
 Route::post('login', [AuthController::class, 'login']);
 
-Route::middleware('auth:api')->get('/products', [AuthController::class, 'getAllProducts']);
+Route::middleware('auth:api')->group(function(){
+    Route::get('/products', [AuthController::class, 'getAllProducts']);
+});
+// Route::middleware('auth:api')->get('/products', [AuthController::class, 'getAllProducts']);
